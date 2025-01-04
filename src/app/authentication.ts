8960b015c9852eed3f6a556fa66fde895a3734c4
Kien Nguyen <nguyenhuutrungkien2105@gmail.com>
2025-01-04 21:57:05 +0700

    resolved jwt error

--- src/app/authentication.ts
+++ src/app/authentication.ts
@@ -63,7 +63,7 @@ export async function getSession() {
     const session = (await cookies()).get("session")?.value;
     
     if (!session) return null;
-    console.log(session)
+
     return await decrypt(session);
   }
   
